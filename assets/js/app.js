0c2b4321bfe6710d00463815dd9a773575c36fd4
Pierre LAURENT <PierreLrt72@gmail.com>
2020-04-22 10:21:48 +0200

    Router

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 
 import '../css/app.css';
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
+import TestPage from "./pages/TestPage";
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
 
 const App = () => {
     return <>
-        <Navbar />
+        <HashRouter>
+            <Navbar />
 
-        <HomePage/>
+            <Switch>
+                <Route path="/test" component={TestPage} />
+                <Route path="/" component={HomePage} />
+            </Switch>
+        </HashRouter>
     </>
 };
 
